362f56fbac1c4c93e9f5abfeb0d67b3067b24681
shahroz.khan <shehroz.altaf92@gmail.com>
2020-01-17 15:21:24 +0500

    MERN

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,18 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use('/api/places', placesRoutes); // => /api/places...
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
+  next();
+});
+
+app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
@@ -23,15 +34,16 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
-    .connect('mongodb+srv://shehroz:abcd1234@cluster0-w0jpl.mongodb.net/places?retryWrites=true&w=majority')
-    .then(() => {
-      app.listen(5000);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
+  .connect(
+    `mongodb+srv://academind:ORlnOPLKvIH9M9hP@cluster0-ntrwp.mongodb.net/mern?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
